2982a82eb777baca143ed3871df16ecdf1f2cfc6
Shu Uesugi <shu@chibicode.com>
2015-02-02 08:47:08 -0800

    Update change_wallpaper.sh

--- change_wallpaper.sh
+++ change_wallpaper.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-mkdir -p ~/Pictures/Wallpapers/.unsplash
-cd ~/Pictures/Wallpapers/.unsplash/
-curl -o image.png "https://unsplash.it/2560/1600/?random"
-osascript -e "tell application \"System Events\" to set picture of every desktop to \"~/Pictures/Wallpapers/.unsplash/image.png\""
+# Use http://launched.zerowidth.com/plists/49c13530-8d26-0132-5958-7a903c0a4b43
+mkdir -p ~/Pictures/Unsplash
+LOGDATE=$(date +%Y%m%dT%H%M%S)
+cd ~/Pictures/Unsplash/
+find . -type f -delete
+curl -o "$LOGDATE.png" "https://unsplash.it/2880/1800/?random"
+osascript -e "tell application \"System Events\" to set picture of every desktop to \"~/Pictures/Unsplash/$LOGDATE.png\""
