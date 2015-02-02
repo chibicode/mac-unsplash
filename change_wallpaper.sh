36b23e85ed5a685bbf46bfc29db4ea0960df65e7
Shu Uesugi <shu@chibicode.com>
2015-02-02 08:47:54 -0800

    Update change_wallpaper.sh

--- change_wallpaper.sh
+++ change_wallpaper.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Use http://launched.zerowidth.com/plists/49c13530-8d26-0132-5958-7a903c0a4b43
+# Must turn off "change wallpaper every X minutes" setting from system prefs
 mkdir -p ~/Pictures/Unsplash
 LOGDATE=$(date +%Y%m%dT%H%M%S)
 cd ~/Pictures/Unsplash/
